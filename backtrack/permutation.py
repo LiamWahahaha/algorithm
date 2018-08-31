ea98124f9f616aa448d6cf3cb5249fbc72badeec
Liam <liliwang@cs.stonybrook.edu>
2018-08-31 16:48:35 -0400

    Add backtracking general algorithm 'backtrack' and its application 'permutation'

---
+++ backtrack/permutation.py
@@ -0,0 +1,21 @@
+from backtrack import Combinatorial_Search
+
+class Permutation(Combinatorial_Search):
+    def is_a_solution(self, current_answer, kth, info):
+        return kth == len(info)
+
+    def generate_candidates(self, current_answer, kth, info):
+        return [candidate for candidate in info if candidate not in current_answer]
+
+    def process_solution(self, current_answer, kth, info):
+        print(current_answer)
+
+    def permute(self, info):
+        self.backtrack([], 0, info)
+
+def main():
+    # case 1
+    Permutation().permute([1, 2, 3])
+
+if __name__ == '__main__':
+    main()
